b13109a88b44c6defdd9f77987fcddde4039ba47
Sergei Nepryakhin <sergey.nepryahin@dashbouquet.com>
2020-11-22 18:17:25 +0300

    feat: move project's description to Readme file

--- src/modules/Layout/index.tsx
+++ src/modules/Layout/index.tsx
@@ -64,20 +64,6 @@ export const Layout: FC = () => {
 
   return (
     <Success>
-      <p>Hello! I want to leave a short comment.</p>
-      <p>
-        In the job description, I saw information that you are trying to switch
-        to Hooks and the Context API. So, to complete the task, I decided to
-        stick with the same state management approach.
-      </p>
-      <p>The Fetch API is used to get data.</p>
-      <p>For flexible styling, I used the CSS-IN-JS library.</p>
-      <p>I used Typescrypt there and also configured basic linting.</p>
-      <p>
-        The character card is rendered using conditional rendering. Routing
-        could also be used for this.
-      </p>
-      <br />
       {isError && <div>Something went wrong ...</div>}
       {visible && (
         <Modal
