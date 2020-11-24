3e8fddcae6888980a3c95652f0a6945b64bc5573
Sergei Nepryakhin <sergey.nepryahin@dashbouquet.com>
2020-11-24 12:52:07 +0300

    feat: add mocking requests and test to async request result

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { App } from './App';
+import { RMApi as Api } from './utils/api';
+
+const RMApi = new Api();
+const mockGetCharPage = (RMApi.getCharactersPage = jest.fn());
 
 test('Test DOM elements', () => {
   const { getByText } = render(<App />);
@@ -15,4 +19,25 @@ test('Test user interactions', () => {
 
   userEvent.click(getByText(/CLICK ME/i));
   getByText('Surprise');
+});
+
+test('Test results of async requests', () => {
+  mockGetCharPage.mockResolvedValueOnce({
+    characterPage: [
+      {
+        id: 1,
+        name: 'aa',
+        gender: 'male',
+        species: null,
+        image: 'aa',
+        episodesList: null,
+        origin: null,
+        location: null,
+      },
+    ],
+    total: 1,
+  });
+  const { getByText } = render(<App />);
+
+  waitFor(() => getByText('aa'));
 });
