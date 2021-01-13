b53a814c1931318a501ac083bee9c0ee1ae83ab3
Sergei Nepryakhin <sergey.nepryahin@dashbouquet.com>
2021-01-13 23:44:20 +0300

    HATT-08 refactor: remove unused div wrapper

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -11,9 +11,7 @@ export const Link: FC<Props> = (props) => {
   const { link } = props;
   return (
     <div>
-      <div>
-        {link.description} ({link.url})
-      </div>
+      {link.description} ({link.url})
     </div>
   );
 };
